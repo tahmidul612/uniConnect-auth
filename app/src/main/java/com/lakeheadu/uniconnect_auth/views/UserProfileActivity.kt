3d6932c3ed5f4bf588d832a7bd9a7334bc4f13db
Rakshit Saxena <rakshit290900@gmail.com>
2021-12-10 16:36:22 -0500

    User Profile Activity (designing done) TODO: (WORK WITH FIREBASE AND ACTIVITY)

---
+++ app/src/main/java/com/lakeheadu/uniconnect_auth/views/UserProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.lakeheadu.uniconnect_auth.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lakeheadu.uniconnect_auth.R
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+}
+}
