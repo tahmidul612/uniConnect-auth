4bd1a77b76eb379bb8ba4d3261e9dc95bb7853e2
Rakshit Saxena <rakshit290900@gmail.com>
2021-12-12 14:44:51 -0500

    intent home sign in

--- app/src/main/java/com/lakeheadu/uniconnect_auth/views/UserProfileActivity.kt
+++ app/src/main/java/com/lakeheadu/uniconnect_auth/views/UserProfileActivity.kt
@@ -3,11 +3,87 @@ package com.lakeheadu.uniconnect_auth.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lakeheadu.uniconnect_auth.R
+import com.lakeheadu.uniconnect_auth.messaging.User
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
 class UserProfileActivity : AppCompatActivity() {
+
+    private lateinit var mUserDetails: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
-}
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+
+    /**
+     * Function to update user profile details to the firestore.
+     */
+   /* private fun updateUserProfileDetails() {
+
+
+
+
+        val userHashMap = HashMap<String, Any>()
+
+        // Get the FirstName from editText and trim the space
+        val firstName = et_first_name.text.toString().trim { it <= ' ' }
+
+
+
+        if (firstName != mUserDetails.firstName) {
+            userHashMap[Constants.FIRST_NAME] = firstName
+        }
+
+        val lastName = et_last_name.text.toString().trim { it <= ' ' }
+        if (lastName != mUserDetails.lastName) {
+            userHashMap[Constants.LAST_NAME] = lastName
+        }
+
+        // Here the field which are not editable needs no update. So, we will update user Mobile Number and Gender for now.
+
+        // Here we get the text from editText and trim the space
+        val mobileNumber = et_mobile_number.text.toString().trim { it <= ' ' }
+
+        val gender = if (rb_male.isChecked) {
+            Constants.MALE
+        } else {
+            Constants.FEMALE
+        }
+
+
+        //User HashMap for Key and Value
+        //Ex: Key: gender, Value: male /or female
+        if (mUserProfileImageURL.isNotEmpty()) {
+            userHashMap[Constants.IMAGE] = mUserProfileImageURL
+        }
+
+
+        if (mobileNumber.isNotEmpty() && mobileNumber!=mUserDetails.mobile.toString()) {
+            userHashMap[Constants.MOBILE] = mobileNumber.toLong()
+        }
+
+        if(gender.isNotEmpty() && gender!=mUserDetails.gender){
+            userHashMap[Constants.GENDER] = gender
+        }
+
+
+        if (mUserDetails.profileCompleted == 0) {
+            userHashMap[Constants.COMPLETE_PROFILE] = 1
+        }
+
+
+        // call the registerUser function of FireStore class to make an entry in the database.
+        FirestoreClass().updateUserProfileData(
+            this@UserProfileActivity,
+            userHashMap
+        )
+    }*/
+
+
+
+
 }
